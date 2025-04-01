721254b391d1cd9fbb127e2b441fda01ec45ccad
Mahsa Mozafari <mahsa.mozafari8585@gmail.com>
2025-04-01 20:43:35 +0330

    premiumControll add

---
+++ src/Controller/PremiumController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import Model.AccountPck.PremiumPackage;
+
+public class PremiumController {
+
+    public void extendSubscription(PremiumPackage packageType){
+
+    }
+
+    public boolean upgradePremiumPackage(PremiumPackage packageType){
+        return false;
+    }
+}
