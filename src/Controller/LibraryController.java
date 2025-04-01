e0c5aec1322d7fd718a252a7ea82054fe3eeaaa7
Mahsa Mozafari <mahsa.mozafari8585@gmail.com>
2025-04-01 20:49:25 +0330

    libraryController add

---
+++ src/Controller/LibraryController.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import Model.AccountPck.User;
+import Model.ContentPck.Content;
+import Model.Playlist;
+
+import java.util.ArrayList;
+
+public class LibraryController {
+
+    public ArrayList<Content> showWatchHistory(){
+        return null;
+    }
+
+    public ArrayList<User> showSubscriptions(){
+        return null;
+    }
+
+    public ArrayList<Playlist> showPlaylists(){
+        return null;
+    }
+
+    public ArrayList<Content> showLikedContents(){
+        return null;
+    }
+}
+
