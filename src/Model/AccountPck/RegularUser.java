d58bad0ca76a22e6c012980c31175d369b3c29c4
Mahsa Mozafari <mahsa.mozafari8585@gmail.com>
2025-04-01 19:15:52 +0330

    regularUser add

---
+++ src/Model/AccountPck/RegularUser.java
@@ -0,0 +1,28 @@
+package Model.AccountPck;
+
+import Model.Category;
+
+import java.util.ArrayList;
+
+public class RegularUser extends User {
+    private final int MAX_CONTENT_PER_PLAYLIST;
+    private final int MAX_PLAYLIST;
+
+    public RegularUser(String username, String fullName, String phoneNumber, String email, ArrayList<Category> favoriteCategories) {
+        super(username, fullName, phoneNumber, email, favoriteCategories);
+        this.MAX_CONTENT_PER_PLAYLIST=10;
+        this.MAX_PLAYLIST=3;
+    }
+
+
+    public int getMAX_CONTENT_PER_PLAYLIST() {
+        return MAX_CONTENT_PER_PLAYLIST;
+    }
+
+    public int getMAX_PLAYLIST() {
+        return MAX_PLAYLIST;
+    }
+
+
+}
+
