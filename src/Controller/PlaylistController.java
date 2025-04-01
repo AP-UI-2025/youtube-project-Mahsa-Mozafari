7bfa82271644b68a9bd345d2c50569a1eb0022e1
Mahsa Mozafari <mahsa.mozafari8585@gmail.com>
2025-04-01 20:41:17 +0330

    playlistControll add

---
+++ src/Controller/PlaylistController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import Model.Playlist;
+
+public class PlaylistController {
+
+    public Playlist createPlaylistForUser(String playlistName){
+        return null;
+    }
+
+    public Playlist createPlaylistForChannel(String playlistName){
+        return null;
+    }
+
+    public Playlist addToPlaylist(int playlistId, int contentId){
+        return null;
+    }
+
+}
+
