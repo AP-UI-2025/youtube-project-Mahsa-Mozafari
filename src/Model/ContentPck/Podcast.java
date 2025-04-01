44866ca2fcbad9615582b6396de30328d3aad573
Mahsa Mozafari <mahsa.mozafari8585@gmail.com>
2025-04-01 18:16:49 +0330

    vid & podcast edit

--- src/Model/ContentPck/Podcast.java
+++ src/Model/ContentPck/Podcast.java
@@ -1,4 +1,20 @@
 package Model.ContentPck;
 
-public class Podcast extends Content{
+import Model.Category;
+
+public class Podcast extends Content {
+    private String creator;
+
+    public Podcast(String title, int contentId, String fileLink, Category category, boolean isExclusive, String creator) {
+        super(title, contentId, fileLink, category, isExclusive);
+        this.creator=creator;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
 }
--- src/Model/ContentPck/Video.java
+++ src/Model/ContentPck/Video.java
@@ -1,4 +1,20 @@
 package Model.ContentPck;
 
+import Model.Category;
+
 public abstract class Video extends Content {
+    private String videoSubtitle;
+
+    public Video(String title, int contentId, String fileLink, Category category, boolean isExclusive) {
+        super(title, contentId, fileLink, category, isExclusive);
+        this.videoSubtitle="";
+    }
+
+    public String getVideoSubtitle() {
+        return videoSubtitle;
+    }
+
+    public void setVideoSubtitle(String videoSubtitle) {
+        this.videoSubtitle = videoSubtitle;
+    }
 }
