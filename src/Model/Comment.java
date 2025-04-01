0d40d2057ba94601253a9e54bdf1cb3d446478fe
Mahsa Mozafari <mahsa.mozafari8585@gmail.com>
2025-04-01 18:10:11 +0330

    comment add

---
+++ src/Model/Comment.java
@@ -0,0 +1,40 @@
+package Model;
+
+import Model.AccountPck.User;
+import java.util.Date;
+
+public class Comment {
+    private User commenter;
+    private String commentText;
+    private Date date;
+
+    public Comment(User commenter) {
+        this.commenter = commenter;
+        this.commentText = "";
+        this.date = new Date();
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public User getCommenter() {
+        return commenter;
+    }
+
+    public void setCommenter(User commenter) {
+        this.commenter = commenter;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
