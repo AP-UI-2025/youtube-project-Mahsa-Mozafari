3d7bce7d3d79e8a67b03beecc987a962d217cf39
Mahsa Mozafari <mahsa.mozafari8585@gmail.com>
2025-04-02 19:38:07 +0330

    edit authControll

--- src/Controller/AuthController.java
+++ src/Controller/AuthController.java
@@ -1,12 +1,15 @@
 package Controller;
 
+import Model.AccountPck.User;
 import Model.Database;
 
 public class AuthController {
     private Database database;
+    private User loggedInUser;
 
     public AuthController() {
         this.database = Database.getInstance();
+        this.loggedInUser=null;
     }
 
     public boolean signUp(String username, String password, String fullName, String email, String phoneNumber, String profileCoverLink) {
@@ -14,11 +17,25 @@ public class AuthController {
     }
 
     public boolean login(String username, String password) {
+        User user=searchForUsername(username);
+        if (user!=null && user.getPassword().equals(password)){
+            loggedInUser=user;
+            return true;
+        }
         return false;
     }
 
     public void logout() {
+        loggedInUser=null;
+    }
 
+    public User searchForUsername(String username){
+        for (User user : database.getUsers()){
+            if(user.getUsername().equals(username)){
+                return user;
+            }
+        }
+        return null;
     }
 
 }
