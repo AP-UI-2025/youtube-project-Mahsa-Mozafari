d76d746eb6fc4990c23e08a8c285a527a8d6ac8e
Mahsa Mozafari <mahsa.mozafari8585@gmail.com>
2025-04-04 21:58:23 +0330

    UserView add

--- src/View/ContentView.java
+++ src/View/ContentView.java
@@ -1,4 +1,16 @@
 package View;
 
+
+import Controller.ChannelController;
+import Controller.ContentController;
+import Model.Database;
+
 public class ContentView {
+    private ContentController contentController;
+    ContentView(){
+        this.contentController= ContentController.getInstance();
+    }
+
+
+
 }
