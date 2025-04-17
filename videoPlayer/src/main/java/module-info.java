a10b05d965f4395f3da2ada5b193f1cadccd135c
Mahsa Mozafari <mahsa.mozafari8585@gmail.com>
2025-04-17 19:47:26 +0330

    1

--- videoPlayer/src/main/java/module-info.java
+++ videoPlayer/src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module com.example.videoplayer {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.media;
+    requires javafx.graphics;
 
     opens com.example.videoplayer to javafx.fxml;
     exports com.example.videoplayer;
+    exports com.example.videoplayer.Model;
+    opens com.example.videoplayer.Model to javafx.fxml;
+    exports com.example.videoplayer.Controller;
+    opens com.example.videoplayer.Controller to javafx.fxml;
 }
