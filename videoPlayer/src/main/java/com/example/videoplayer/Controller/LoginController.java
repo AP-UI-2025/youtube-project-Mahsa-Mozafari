b58de945d2d5e6201ae086003b5bdee90c0bf655
Mahsa Mozafari <mahsa.mozafari8585@gmail.com>
2025-04-18 12:01:05 +0330

    edit view login

--- videoPlayer/src/main/java/com/example/videoplayer/Controller/LoginController.java
+++ videoPlayer/src/main/java/com/example/videoplayer/Controller/LoginController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -18,11 +19,15 @@ public static Stage ctrlStage;
     private PasswordField passwordText;
 
     @FXML
+    private Label messageLabel;
+
+
+    @FXML
     private TextField usernameText;
 
     @FXML
     void backToStart(ActionEvent event)throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/videoplayer/main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         ctrlStage.setScene(scene);
         ctrlStage.show();
@@ -31,11 +36,16 @@ public static Stage ctrlStage;
     @FXML
     void loginAct(ActionEvent event) throws IOException {
         String result = AuthController.getInstance().login(usernameText.getText(), passwordText.getText());
+
         if (result.startsWith("success")) {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/videoplayer/main-view.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 680, 480);
             ctrlStage.setScene(scene);
             ctrlStage.show();
+        } else if (result.equals("banned_user")) {
+            messageLabel.setText("You are banned.");
+        } else if (result.equals("invalid")) {
+            messageLabel.setText("Invalid username or password");
         }
     }
 }
